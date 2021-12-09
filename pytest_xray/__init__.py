47219e52afee3d46216ebc761ea99ae855405d47
Knysh <ilyaknyshv@gmail.com>
2021-12-09 16:43:22 +0200

    Update __init__.py

--- pytest_xray/__init__.py
+++ pytest_xray/__init__.py
@@ -1,2 +1,17 @@
-__version__ = "0.2.1"
+__version__ = "0.3.1"
 
+
+class Execution:
+    def __init__(self):
+        self._execution_id = ''
+
+    @property
+    def execution_id(self):
+        return self._execution_id
+
+    @execution_id.setter
+    def execution_id(self, value):
+        self._execution_id = value
+
+
+execution = Execution()
